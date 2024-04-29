ab1a534d4e0da8c7c1c877e2f63bb231cdfed56a
meggie0718 <150638437+meggie0718@users.noreply.github.com>
2024-04-29 10:35:50 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,8 +6,8 @@ var span=10
 function setup() {
   createCanvas(400, 400);
   capture = createCapture(VIDEO)  //啟動攝影機
-  capture.size(capture_width,capture_heigh); //設定顯示畫面大小
-  captureGraphics=captureGraphics(capture_width,capture_heigh)
+  capture.size(capture_width,capture_height); //設定顯示畫面大小
+  captureGraphics=captureGraphics(capture_width,capture_height)
   captureGraphics.translate(capture_width,0)
   captureGraphics.scale(-1,1)
   capture.hide()
@@ -15,11 +15,13 @@ function setup() {
 
 function draw() {
   background(220);
+  noStroke()
+
   push()
     translate(width/2-320,height/2-240)
     captureGraphics.image(capture,0,0)
-    for(var x=0;x<captureGraphics_width;x=x+span){
-      for(var y=0;y<captureGraphics_height;y=y+span){
+    for(var x=0;x<captureGraphics.width;x=x+span){
+      for(var y=0;y<captureGraphics.height;y=y+span){
         var pixel=captureGraphics.get(x,y)
         fill(pixel)
         rect(x,y,span)
