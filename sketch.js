472409f2ec46029178794a796d5cb5b9ba6dc493
meggie0718 <150638437+meggie0718@users.noreply.github.com>
2024-04-29 11:11:37 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,32 @@
+var captureGraphics
+var capture_width=640
+var capture_height=480
+var span=10
+
+function setup() {
+  createCanvas(windowWidth, windowHeight);
+  capture = createCapture(VIDEO)  //啟動攝影機
+  capture.size(capture_width,capture_height); //設定顯示畫面大小
+  captureGraphics=createGraphics(capture_width,capture_height)
+  captureGraphics.translate(capture_width,0)
+  captureGraphics.scale(-1,1)
+  capture.hide()
+}
+
+function draw() {
+  background(220);
+  noStroke()
+
+  push()
+    translate(width/2-320,height/2-240)
+    captureGraphics.image(capture,0,0)
+    for(var x=0;x<captureGraphics.width;x=x+span){
+      for(var y=0;y<captureGraphics.height;y=y+span){
+        var pixel=captureGraphics.get(x,y)
+        fill(pixel)
+        rect(x,y,span)
+      }
+    }
+  pop()
+
+}
