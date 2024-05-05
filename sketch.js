bbac094313c7ea83bca4038ce2f7009693ba8ad0
meggie0718 <150638437+meggie0718@users.noreply.github.com>
2024-05-06 07:53:37 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -2,6 +2,7 @@ var captureGraphics
 var capture_width=640
 var capture_height=480
 var span=10
+var radioElement
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -11,12 +12,20 @@ function setup() {
   captureGraphics.translate(capture_width,0)
   captureGraphics.scale(-1,1)
   capture.hide()
+  //選紐
+  radioElement = createRadio()
+  radioElement.position(width/2-300,20)
+  radioElement.option("方塊")
+  radioElement.option("圓圈")
+  radioElement.option("亮度")
+  radioElement.style("color","#fff")
+  radioElement.style("font-size","35px")
 }
 
 function draw() {
   background(220);
   noStroke()
-
+  span=5+map(mouseX,0,width,0,20)
   push()
     translate(width/2-320,height/2-240)
     captureGraphics.image(capture,0,0)
@@ -24,7 +33,17 @@ function draw() {
       for(var y=0;y<captureGraphics.height;y=y+span){
         var pixel=captureGraphics.get(x,y)
         fill(pixel)
-        rect(x,y,span)
+        if(radioElement.value()=="方塊"||radioElement.value()==""){
+          rect(x,y,span)
+        }
+        if(radioElement.value()=="圓圈"){
+          ellipse(x,y,span)
+        }
+        if(radioElement.value()=="亮度"){
+          bk=(pixel[0]+pixel[1]+pixel[2])/3
+          fill(bk)
+          ellipse(x,y,span*map(bk,0,255,0,1))
+        }
       }
     }
   pop()
